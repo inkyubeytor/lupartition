f4a8804bb3aa779d672c910be388653aa2bf0673
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2021-07-23 00:00:19 -0400

    working all partitions - tested on naive partition tests

--- src/lupartition/methods/naive.py
+++ src/lupartition/methods/naive.py
@@ -190,6 +190,112 @@ def naive_partition(tree, key, parts, lower, upper):
     return assignment
 
 
+def naive_partition_all(tree, key, parts, lower, upper):
+    """
+    Implements `partition` with the naive O(p^4 u^2 n) algorithm.
+    """
+    dp_tree = copy_partition(tree, key)
+
+    # dp_tree                          - whole tree
+    # dp_tree.nodes[v]                 - vertex v
+    # dp_tree.nodes[v]["table"][index i] - vertex v, child i is a dict with values "vertex": v_i,
+    #   "parts": k -> Dict { z -> Set{ (z', k') for s1 | (None, k') for s2 } } }
+
+    # Traverse the graph bottom up, defining children as
+    # already-processed neighbors
+    processed = set()
+    root = None
+    for v in nx.dfs_postorder_nodes(dp_tree):
+        children = set(dp_tree.neighbors(v)) & processed
+        parts_0 = {k: {} for k in range(2, parts + 1)}
+        parts_0[1] = {dp_tree.nodes[v]["weight"]: {(None, 0)}}
+        dp_tree.nodes[v]["table"] = [{"vertex": None, "parts": parts_0}]
+        for child in children:
+            parts_dict = {}
+            for k in range(1, parts + 1):
+                z_dict = {}
+                # S1 stuff
+                for k_prime in range(1, k + 1):
+                    left = dp_tree.nodes[v]["table"][-1]["parts"][k_prime]
+                    right = dp_tree.nodes[child]["table"][-1]["parts"][
+                        k - k_prime + 1]
+                    for a in left:
+                        for b in right:
+                            try:
+                                z_dict[a + b].add((a, k_prime))
+                            except KeyError:
+                                z_dict[a + b] = {(a, k_prime)}
+                # S2 stuff
+                for k_prime in range(1, k):
+                    left = dp_tree.nodes[v]["table"][-1]["parts"][k_prime]
+                    right = dp_tree.nodes[child]["table"][-1]["parts"][
+                        k - k_prime]
+                    for b in right:
+                        if lower <= b <= upper:
+                            for a in left:
+                                try:
+                                    z_dict[a].add((None, k_prime))
+                                except KeyError:
+                                    z_dict[a] = {(None, k_prime)}
+                parts_dict[k] = z_dict
+            dp_tree.nodes[v]["table"].append(
+                {"vertex": child, "parts": parts_dict})
+        processed.add(v)
+        root = v
+
+    # backtracking
+    try:
+        z = next(filter(lambda y: lower <= y <= upper,
+                        dp_tree.nodes[root]["table"][-1]["parts"][parts]))
+    except StopIteration:
+        return None
+
+    start = {root: 0}
+    input_queue = \
+        [(root, None, parts, len(dp_tree.nodes[root]["table"]) - 1, 0)]
+    queue = [(start, input_queue)]
+
+    def process(assignment, assignment_queue):
+        try:
+            v, z, k, i, v_part_num = assignment_queue.pop()
+        except IndexError:
+            return assignment
+        if i == 0:
+            queue.append((assignment, assignment_queue))
+            return
+
+        z_values = [z] if z is not None else \
+            [y for y in dp_tree.nodes[v]["table"][i]["parts"][k]
+             if lower <= y <= upper]
+
+        vp = dp_tree.nodes[v]["table"][i]["vertex"]
+        for z in z_values:
+            for zp, kp in dp_tree.nodes[v]["table"][i]["parts"][k][z]:
+                new_a = assignment.copy()
+                new_q = assignment_queue.copy()
+                if zp is None:
+                    new_part_num = 1 + max(new_a.values(), default=0)
+                    new_a[vp] = new_part_num
+                    new_q.append((v, z, kp, i - 1, v_part_num))
+                    new_q.append((vp, None, k - kp,
+                                  len(dp_tree.nodes[vp]["table"]) - 1,
+                                  new_part_num))
+                else:
+                    new_a[vp] = v_part_num
+                    new_q.append((v, zp, kp, i - 1, v_part_num))
+                    new_q.append((vp, z - zp, k - kp + 1,
+                                  len(dp_tree.nodes[vp]["table"]) - 1,
+                                  v_part_num))
+                queue.append((new_a, new_q))
+
+    outputs = []
+    while queue:
+        if assignment := process(*queue.pop()):
+            outputs.append(assignment)
+
+    return outputs
+
+
 def naive_decision(tree, key, parts, lower, upper):
     """
     Implements the decision problem variant of the naive O(p^4 u^2 n) algorithm.
