3d8bc0e0edec0ba2a9b5191dfe6b6a752d01b1a2
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2021-07-22 23:21:19 -0400

    updated hashing with no initial value but caching

--- src/lupartition/methods/frozendict.py
+++ src/lupartition/methods/frozendict.py
@@ -1,9 +1,11 @@
 class FrozenDict(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.hash_value = hash(frozenset(self.items()))
+        self.hash_value = None
 
     def __hash__(self):
+        if self.hash_value is None:
+            self.hash_value = hash(frozenset(self.items()))
         return self.hash_value
 
     def __setitem__(self, key, value):
