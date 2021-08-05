58d9aacc276e980c53abb48ee920ff458d61b458
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2021-08-05 18:52:48 -0500

    added helper function for running partition tests

--- tests/test_naive.py
+++ tests/test_naive.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.lupartition.methods.naive import naive_decision, naive_partition
 from tests.utils import check_partition
 
@@ -27,10 +29,15 @@ def test_naive_decision_tree3(tree3):
     assert not naive_decision(tree3, "mass", 7, 0.5, 5.5)
 
 
+def check_naive_partition(graph, key, parts, lower, upper):
+    partition = naive_partition(graph, key, parts, lower, upper)
+    return check_partition(graph, key, parts, lower, upper, partition)
+
+
 def test_naive_partition_tree1(tree1):
     # Positive tests
-    assert check_partition(tree1, "weight", 3, 4, 6, naive_partition(tree1, "weight", 3, 4, 6))
-    assert check_partition(tree1, "weight", 2, 5, 10, naive_partition(tree1, "weight", 2, 5, 10))
+    assert check_naive_partition(tree1, "weight", 3, 4, 6)
+    assert check_naive_partition(tree1, "weight", 2, 5, 10)
     # Negative tests
     assert not naive_partition(tree1, "weight", 1, 4, 6)
     assert not naive_partition(tree1, "weight", 4, 2, 3)
@@ -39,14 +46,14 @@ def test_naive_partition_tree1(tree1):
 
 def test_naive_partition_tree2(tree2):
     # Positive tests
-    assert check_partition(tree2, "weight", 15, 1, 2, naive_partition(tree2, "weight", 15, 1, 2))
-    assert check_partition(tree2, "weight", 5, 3, 3, naive_partition(tree2, "weight", 5, 3, 3))
+    assert check_naive_partition(tree2, "weight", 15, 1, 2)
+    assert check_naive_partition(tree2, "weight", 5, 3, 3)
     # Negative tests
     assert not naive_partition(tree2, "weight", 4, 2, 3)
 
 
 def test_naive_partition_tree3(tree3):
     # Positive tests
-    assert check_partition(tree3, "mass", 3, 5.5, 8.5, naive_partition(tree3, "mass", 3, 5.5, 8.5))
+    assert check_naive_partition(tree3, "mass", 3, 5.5, 8.5)
     # Negative tests
     assert not naive_partition(tree3, "mass", 7, 0.5, 5.5)
