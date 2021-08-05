4f45d9ba4bd00d38b1e4bbe50ece4cd811a9c14b
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2021-08-05 18:45:33 -0500

    reintroduced use of text fixtures

--- tests/test_naive.py
+++ tests/test_naive.py
@@ -1,9 +1,8 @@
 from src.lupartition.methods.naive import naive_decision, naive_partition
-from tests.test_graphs import tree1, tree2, tree3
 from tests.utils import check_partition
 
 
-def test_naive_decision_tree1():
+def test_naive_decision_tree1(tree1):
     # Positive tests
     assert naive_decision(tree1, "weight", 3, 4, 6)
     assert naive_decision(tree1, "weight", 2, 5, 10)
@@ -13,7 +12,7 @@ def test_naive_decision_tree1():
     assert not naive_decision(tree1, "weight", 5, 2, 3)
 
 
-def test_naive_decision_tree2():
+def test_naive_decision_tree2(tree2):
     # Positive tests
     assert naive_decision(tree2, "weight", 15, 1, 2)
     assert naive_decision(tree2, "weight", 5, 3, 3)
@@ -21,20 +20,14 @@ def test_naive_decision_tree2():
     assert not naive_decision(tree2, "weight", 4, 2, 3)
 
 
-def test_naive_decision_tree3():
+def test_naive_decision_tree3(tree3):
     # Positive tests
     assert naive_decision(tree3, "mass", 3, 5.5, 8.5)
     # Negative tests
     assert not naive_decision(tree3, "mass", 7, 0.5, 5.5)
 
 
-def test_naive_decision():
-    test_naive_decision_tree1()
-    test_naive_decision_tree2()
-    test_naive_decision_tree3()
-
-
-def test_naive_partition_tree1():
+def test_naive_partition_tree1(tree1):
     # Positive tests
     assert check_partition(tree1, "weight", 3, 4, 6, naive_partition(tree1, "weight", 3, 4, 6))
     assert check_partition(tree1, "weight", 2, 5, 10, naive_partition(tree1, "weight", 2, 5, 10))
@@ -44,7 +37,7 @@ def test_naive_partition_tree1():
     assert not naive_partition(tree1, "weight", 5, 2, 3)
 
 
-def test_naive_partition_tree2():
+def test_naive_partition_tree2(tree2):
     # Positive tests
     assert check_partition(tree2, "weight", 15, 1, 2, naive_partition(tree2, "weight", 15, 1, 2))
     assert check_partition(tree2, "weight", 5, 3, 3, naive_partition(tree2, "weight", 5, 3, 3))
@@ -52,19 +45,8 @@ def test_naive_partition_tree2():
     assert not naive_partition(tree2, "weight", 4, 2, 3)
 
 
-def test_naive_partition_tree3():
+def test_naive_partition_tree3(tree3):
     # Positive tests
     assert check_partition(tree3, "mass", 3, 5.5, 8.5, naive_partition(tree3, "mass", 3, 5.5, 8.5))
     # Negative tests
     assert not naive_partition(tree3, "mass", 7, 0.5, 5.5)
-
-
-def test_naive_partition():
-    test_naive_partition_tree1()
-    test_naive_partition_tree2()
-    test_naive_partition_tree3()
-
-
-if __name__ == "__main__":
-    test_naive_decision()
-    test_naive_partition()
